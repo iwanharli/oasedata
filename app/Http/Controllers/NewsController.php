b73423d0d8103a213f99ca574680750a41b35b62
Bima Indra <121376853+darkestdark4@users.noreply.github.com>
2024-02-26 15:10:07 +0700

    update news and statistic controller

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -7,25 +7,22 @@ use TCG\Voyager\Models\Category;
 
 use Illuminate\Http\Request;
 
-class NewsController extends Controller
-{
-    public function index()
-    {
-            $all_news       = Post::orderBy('created_at', 'desc')->skip(0)->get();
-            $random_news    = Post::inRandomOrder()->take(5)->get();
-            $categories     = Category::all();
-            $categoriesWithCount = Category::withCount('posts')->orderByDesc('posts_count')->get();
+class NewsController extends Controller {
+    public function index() {
+        $all_news = Post::paginate(5);
+        $random_news = Post::inRandomOrder()->take(5)->get();
+        $categories = Category::all();
+        $categoriesWithCount = Category::withCount('posts')->orderByDesc('posts_count')->get();
 
-            // var_dump($all_news);
+        // var_dump($all_news);
 
-            return view('_news.main-list', compact('all_news', 'random_news', 'categoriesWithCount'));
+        return view('_news.main-list', compact('all_news', 'random_news', 'categoriesWithCount'));
     }
 
 
-    public function show($slug)
-    {
+    public function show($slug) {
         $post = Post::where('slug', $slug)->firstOrFail();
-        $random_news = Post::inRandomOrder()->take(5)->get();
+        $random_news = Post::whereNot('slug', $slug)->inRandomOrder()->take(5)->get();
 
         $title  = $post->title;
         $desc   = $post->excerpt;
--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -2,27 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use TCG\Voyager\Models\Post;
+use App\Models\Statistic;
 use TCG\Voyager\Models\Category;
 
 use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
-    public function index()
-    {
-        $all_news = Post::orderBy('created_at', 'desc')->skip(0)->take(10)->get();
+	public function index()
+	{
+		$statistics = Statistic::paginate(5);
+		$randomStats = Statistic::inRandomOrder()->take(5)->get();
 
-        return view('_statistic.main_list');
-    }
-
-
-    public function show($slug)
-    {
-        $post = Post::where('slug', $slug)->first();
-        $latests = Post::orderBy('created_at', 'desc')->skip(0)->take(10)->get();
+		return view('_statistic.main_list', compact('statistics', 'randomStats'));
+	}
 
 
-        return view('_news.content', $latests);
-    }
+	public function show($slug)
+	{
+		$statistic = Statistic::where('slug', $slug)->first();
+		$randomStats = Statistic::whereNot('slug', $slug)->inRandomOrder()->take(5)->get();
+
+		return view('_statistic.single-post', compact('statistic', 'randomStats'));
+	}
 }
